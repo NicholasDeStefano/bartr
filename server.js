5f9e6540fb897fd79a4a52720be3edefb9de32cc
Nicholas DeStefano <nicholas@mashwork.com>
2014-09-23 00:28:28 -0400

    added mongolab

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var uriUtil = require('mongodb-uri');
 var cors = require("cors");
 var passport = require("passport");
 var flash = require("connect-flash");
@@ -13,8 +14,13 @@ var session = require("express-session");
 
 var config = require('./config/config')[env];
 
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
 var port = process.env.PORT || 1529;
-mongoose.connect(config.db);
+var mongodbUri = 'mongodb://heroku_app29898300:cqv8c19fainnkmokt5ldobb57d@ds039000.mongolab.com:39000/heroku_app29898300';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+mongoose.connect(mongooseUri, options);
 
 require('./config/passport')(passport);
 
