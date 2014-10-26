dae80bf44559904dbd068538412f0133f949fd0c
Nicholas DeStefano <nicholas@mashwork.com>
2014-10-26 02:28:46 -0400

    trying to get the upload button to work on mobile

--- public/js/controllers/IndexCtrl.js
+++ public/js/controllers/IndexCtrl.js
@@ -13,21 +13,5 @@
         $scope.backFeed = true;
       }
     });
-    // if($location.path() === '/login') {
-    //   console.log("login page");
-    //   $scope.indexTest = "hideNav";
-    // } else if($location.path() === '/app'){
-    //   console.log("app page");
-    //   $scope.indexTest = "showNav";
-    // } else if($location.path() === '/likes'){
-    //   console.log("likes page");
-    //   $scope.indexTest = "showNav";
-    // }
-
-
-
-
-
-
   })
 })();
