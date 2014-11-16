b0e1f69da5af2529776bf95208f89852093926d2
Nicholas DeStefano <nicholas@mashwork.com>
2014-11-16 03:41:51 -0500

    mobile test #6

--- public/js/controllers/posts/CreatePostsCtrl.js
+++ public/js/controllers/posts/CreatePostsCtrl.js
@@ -20,7 +20,7 @@
           });
 
         function uploadImage(file) {
-            $scope.page.mobileTest = file;
+            
             var name = Math.round(Math.random()*10000) + '$' + file.name;
             delete file.name;
             file['name'] = name;
@@ -70,18 +70,49 @@
                 $scope.page.error = "You need a title, image, and caption.";
             } else {
             $scope.page.mobileTest = "it seems all the properties are accounted for.";
-            uploadImage($scope.image);
-            // console.log("image", $scope.image);
-            // $scope.post.user = $scope.user._id;
-            // $scope.post.imgRef = $scope.image.name;
-            // console.log("post about to send", post);
-            // $http.post('/api/posts', $scope.post).success(function(response) {
-            //     console.log("successfull res", response);
-            // }).then(function(){
-            //     $location.path('/app');
-            // })
-            
-            // $scope.post = {};
+            var file = $scope.image;
+            var name = Math.round(Math.random()*10000) + '$' + file.name;
+            delete file.name;
+            file['name'] = name;
+            $http.get('/api/s3Policy?mimeType='+ file.type).success(function(response){
+                $scope.page.mobileTest = "get request is being made";
+
+                var s3Params = response;
+                $scope.image = $upload.upload({
+                    url: 'https://bartr-imgs.s3.amazonaws.com/',
+                    method: 'POST',
+                    data: {
+                        'key' : 'uploads/' + file.name,
+                        'acl' : 'public-read',
+                        'Content-Type' : file.type,
+                        'AWSAccessKeyId': s3Params.AWSAccessKeyId,
+                        'success_action_status' : 'http://localhost:1529/#/app',
+                        'Policy' : s3Params.s3Policy,
+                        'Signature' : s3Params.s3Signature
+                    },
+                    file: file,
+                }).progress(function(evt) {
+                    $scope.page.upProg = true;
+                    $scope.page.progress = parseInt(100.0 * evt.loaded / evt.total);
+                }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    $scope.page.mobileTest = "file was uploaded sucessfully";
+
+                    $scope.post.user = $scope.user._id;
+                    $scope.post.imgRef = config.file.name;
+                    $http.post('/api/posts', $scope.post).success(function(response) {
+                        console.log("successfull res", response);
+                    }).then(function(){
+                        $location.path('/app');
+                    })
+                }).error(function(){
+                $scope.page.mobileTest = "Something went wrong.";
+
+                    $scope.page.wrong = "Something went wrong...";
+                })
+            }).error(function(){
+                $scope.page.mobileTest = "Something went wrong.";
+            })
             }
         }
 
